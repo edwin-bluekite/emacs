150461498bf55b53691e8a1011b2c0ca2d164237
Edwin Allen <edwin@bluekite.com>
2013-10-13 08:34:03 -0600

    Add markdown-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -48,3 +48,10 @@
 ;; w3m
 (require 'w3m-load)
 (setq w3m-use-cookies t)
+
+;;Markdown
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
